4053f0821d3947672b2691c27f59ea2c9eafec05
StraetenMax <maxstraeten@yahoo.com>
2017-05-14 16:46:06 +0200

    finalisation id

--- src/application/Test.java
+++ src/application/Test.java
@@ -6,13 +6,13 @@ import definitionRectangle.Rectangle;
 public class Test {
 
 	public static void main(String[] args) {
-	Rectangle rectangles1 = new Rectangle(12.6, 5.3);
-	Rectangle rectangles2 = new Rectangle(3.0, 3.0);
-	Rectangle rectangles3 = new Rectangle(10.5, 5.0);
+	Rectangle[] rectangles = new Rectangle[3];
+	rectangles[0] = new Rectangle(12.6, 5.3);
+	rectangles[1] = new Rectangle(3.0, 3.0);
+	rectangles[2] = new Rectangle(10.5, 5.0);
 	
-	System.out.println(rectangles1);
-	System.out.println(rectangles2);
-	System.out.println(rectangles3);
+	for(Rectangle resultat : rectangles){
+	System.out.println(resultat);
 	}
-
+	}
 }
