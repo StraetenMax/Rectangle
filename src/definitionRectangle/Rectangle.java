658e4470ddef2ab1ac0af45109d124358ce0bf0b
StraetenMax <maxstraeten@yahoo.com>
2017-05-14 15:35:44 +0200

    constructeur mutateur

--- src/definitionRectangle/Rectangle.java
+++ src/definitionRectangle/Rectangle.java
@@ -1,5 +1,34 @@
 package definitionRectangle;
 
 public class Rectangle {
-
+	//propriétés
+	private double longueur;
+	private double largeur;
+	
+	//constructeur
+	public Rectangle(){
+		longueur = 00.00;
+		largeur = 00.00;		
+	}
+	public Rectangle(double longueur, double lageur){
+		this.longueur =longueur;
+		this.largeur =largeur;
+	}
+	
+	//assecceur et mutateur
+	public double getLongeur(){
+		return longueur;
+	}
+	public double getLargeur(){
+		return largeur;
+	}
+	public void setLongueur(double longueur){
+		this.longueur= longueur;
+	}
+	public void setLargeur(double largeur){
+		this.largeur= largeur;
+	}
+	
+	//methodes
+	
 }
